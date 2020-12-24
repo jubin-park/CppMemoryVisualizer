d87754ad6254222980a722fdfc83062b6bc9ebda
Jubin Park <jubin-park@naver.com>
2020-12-24 12:24:22 +0900

    add: WindowTitleConverter

---
+++ Views/WindowTitleConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CppMemoryVisualizer.Views
+{
+    class WindowTitleConverter : IValueConverter
+    {
+        private static string WINDOW_TITLE = "C/C++ Memory Visualizer";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string srcPath = value as string;
+
+            if (srcPath == null || srcPath.Trim() == string.Empty)
+            {
+                return WINDOW_TITLE;
+            }
+
+            return string.Format("{0} - @[{1}]", WINDOW_TITLE, srcPath);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return WINDOW_TITLE;
+        }
+    }
+}
